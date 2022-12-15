f115f2ad5b2d710071ec080d3269869e11688b77
apoorv4702 <100695351+apoorv4702@users.noreply.github.com>
2022-12-15 11:56:23 +0530

    Add files via upload

---
+++ build/precache-manifest.2ab13a70165f552beba2efd1fdff4ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9321e4f494ac0cc6db7",
+    "url": "/Todo-List/static/css/main.4a96d7a3.chunk.css"
+  },
+  {
+    "revision": "f9321e4f494ac0cc6db7",
+    "url": "/Todo-List/static/js/main.f9321e4f.chunk.js"
+  },
+  {
+    "revision": "0765819dcae452713017",
+    "url": "/Todo-List/static/js/1.0765819d.chunk.js"
+  },
+  {
+    "revision": "b03d2371a89926149170",
+    "url": "/Todo-List/static/js/2.b03d2371.chunk.js"
+  },
+  {
+    "revision": "942af2a6b0cbf7002ac7",
+    "url": "/Todo-List/static/js/runtime~main.942af2a6.js"
+  },
+  {
+    "revision": "785793b6e647951aff816d1d1623d6da",
+    "url": "/Todo-List/index.html"
+  }
+];
